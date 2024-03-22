2249e8a43f6d1a950657fe23997423e1e5ee7cb4
Festus Kai Mpita <kentkai32@gmail.com>
2024-03-22 23:17:01 +0300

    added a few lines of code, got stuck somewhere

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride >= 401 && ride <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride >= 2501) {
+    return scuberGreetingForFeet();
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(_NYC) {
+  return "Ok, sounds good.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(_generous) {
+  return "Thank you so much.";
 }
